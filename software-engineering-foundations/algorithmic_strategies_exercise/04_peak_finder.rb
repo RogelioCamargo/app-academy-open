93ef7c7b6e4489442d36ea6a428ed9c2de6a4bc7
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-02-28 21:53:06 -0800

    Write peak_finder method

--- software-engineering-foundations/algorithmic_strategies_exercise/04_peak_finder.rb
+++ software-engineering-foundations/algorithmic_strategies_exercise/04_peak_finder.rb
@@ -3,6 +3,20 @@
 # An element is considered a "peak" if it is greater than both it's left and right neighbor.
 # The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
 
+def peak_finder(arr) 
+	return arr if arr.length == 0 || arr.length == 1
+
+	peaks = []
+	peaks << arr[0] if arr[0] > arr[1]
+
+	(1...arr.length - 1).each do |idx|
+		peaks << arr[idx] if arr[idx] > arr[idx - 1] && arr[idx] > arr[idx + 1]
+	end
+
+	peaks << arr[-1] if arr[-1] > arr[-2]
+
+	peaks
+end
 
 p peak_finder([1, 3, 5, 4])         # => [5]
 p peak_finder([4, 2, 3, 6, 10])     # => [4, 10]
