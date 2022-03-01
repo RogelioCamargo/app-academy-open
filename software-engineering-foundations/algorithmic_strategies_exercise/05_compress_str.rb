362df78fd824a42beefd68825e13f4af9cd8430f
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-01 13:58:19 -0800

    Write compress method

--- software-engineering-foundations/algorithmic_strategies_exercise/05_compress_str.rb
+++ software-engineering-foundations/algorithmic_strategies_exercise/05_compress_str.rb
@@ -2,6 +2,24 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 
+def compress_str(str) 
+	new_str = ""
+	i = 0
+	while i < str.length 
+		char = str[i]
+		count = 0
+		j = i
+		while char == str[j]
+			count += 1
+			j += 1
+		end
+		new_str += count == 1 ? char : count.to_s + char
+		i += count
+	end
+
+	new_str
+
+end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
