8c822f83f3ce51ca20e67f52f05275fa60a5e7ad
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-02 16:41:46 -0800

    Finish debugging titleize mthod

--- software-engineering-foundations/debugging_exercise_1/lib/titleize.rb
+++ software-engineering-foundations/debugging_exercise_1/lib/titleize.rb
@@ -6,13 +6,13 @@
 
 require "byebug"
 
-little_words = [ "and", "the", "over", "a", "on", "of" ]
+LITTLE_WORDS = [ "and", "the", "over", "a", "on", "of" ]
 
 def titleize(title)
   words = title.split(" ")
 
-  titleized_words = words.map.with_index do |i, word|
-    if i == 0 || !little_words.include?(word)
+  titleized_words = words.map.with_index do |word, i|
+    if i == 0 || !LITTLE_WORDS.include?(word)
       word.capitalize
     else
       word.downcase
@@ -21,3 +21,5 @@ def titleize(title)
 
   titleized_words.join(" ")
 end
+
+# debugger
