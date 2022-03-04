8dfd5797733856ad3600ab4ccf2a89d43d757332
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-04 08:51:39 -0800

    Write methods to complete guessing game

--- software-engineering-foundations/guessing_game_project/lib/guessing_game.rb
+++ software-engineering-foundations/guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,37 @@
 class GuessingGame
+	def initialize(min, max)
+		@min = min
+		@max = max
+		@secret_num = rand(min..max)
+		@num_attempts = 0
+		@game_over = false
+	end
 
+	def num_attempts
+		@num_attempts
+	end
+
+	def game_over?
+		@game_over
+	end
+
+	def check_num(guess)
+		@num_attempts += 1
+
+		if guess > @secret_num
+			puts "too big"
+		elsif guess < @secret_num
+			puts "too small"
+		else
+			@game_over = true
+			puts "you win"
+		end
+	end
+
+	def ask_user
+		puts "enter a number"
+
+		guess = gets.chomp.to_i
+		self.check_num(guess)
+	end
 end
