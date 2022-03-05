6b8d4da1383e4bdf2d6fb7c0539d29cf25575e17
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-05 06:39:28 -0800

    Write Code class

--- software-engineering-foundations/mastermind_project/lib/code.rb
+++ software-engineering-foundations/mastermind_project/lib/code.rb
@@ -6,4 +6,37 @@ class Code
     "Y" => :yellow
   }
 
+	attr_reader :pegs
+
+	def self.valid_pegs?(pegs)
+		pegs.each { |char| return false if !POSSIBLE_PEGS.has_key?(char.upcase) }
+
+		true
+	end
+
+	def self.random(length)
+		pegs = Array.new(length) { POSSIBLE_PEGS.keys.sample }
+		
+		Code.new(pegs)
+	end
+
+	def self.from_string(string)
+		Code.new(string.split(""))
+	end
+
+	def initialize(pegs)
+		if Code.valid_pegs?(pegs)
+			@pegs = pegs.map(&:upcase)
+		else
+			raise "invalid pegs"
+		end
+	end
+
+	def [](position)
+		@pegs[position]
+	end
+
+	def length
+		@pegs.length
+	end
 end
