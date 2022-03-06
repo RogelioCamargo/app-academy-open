5393128fab4eb3f3382e06ce80bbb6a5e353796b
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-05 17:36:22 -0800

    Complete recap problemset

--- software-engineering-foundations/recap_exercise_1/lib/problems.rb
+++ software-engineering-foundations/recap_exercise_1/lib/problems.rb
@@ -6,7 +6,19 @@
 #
 # all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
 def all_vowel_pairs(words)
+	pairs = []
+	vowels = ["a", "e", "i", "o", "u"]
+	
+	(0...words.length).each do |i|
+		(i + 1...words.length).each do |j|
+			next if i == j
+			
+			pair = words[i] + " " + words[j]
+			pairs << pair if vowels.all? { |vowel| pair.include?(vowel) }
+		end
+	end
 
+	pairs
 end
 
 
@@ -18,7 +30,9 @@ end
 # composite?(9)     # => true
 # composite?(13)    # => false
 def composite?(num)
+	(2...num).each { |i| return true if num % i == 0 }
 
+	false
 end
 
 
@@ -32,7 +46,7 @@ end
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
-
+	bigrams.select { |bigram| str.match?(/#{bigram}/) }
 end
 
 class Hash
@@ -50,7 +64,14 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
+			prc ||= Proc.new { |k, v| k == v }
+			hash = {}
 
+			self.each do |k, v|
+				hash[k] = v if prc.call(k, v)
+			end
+
+			hash
     end
 end
 
@@ -64,7 +85,17 @@ class String
     # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
     # "cats".substrings(2)  # => ["ca", "at", "ts"]
     def substrings(length = nil)
+			# length ||= self.length
+			substrings = []
 
+			(0...self.length).each do |i|
+				(i...self.length).each do |j|
+					substring = self[i..j]
+					substrings << substring
+				end
+			end
+
+			length ? substrings.select { |w| w.length == length } : substrings
     end
 
 
@@ -78,6 +109,15 @@ class String
     # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
     # "zebra".caesar_cipher(4)    #=> "difve"
     def caesar_cipher(num)
+			alphabet = ("a".."z").to_a
+			encoded = ""
+			
+			self.each_char do |char|
+				idx = alphabet.index(char)
+				new_idx = (idx + num) % 26
+				encoded += alphabet[new_idx]
+			end
 
+			encoded
     end
 end
