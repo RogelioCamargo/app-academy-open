48e12db3658881faa26e0f1cec1e6ac91f5781ed
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-12 07:41:47 -0800

    Write Tile class

--- ruby/sudoku_project/tile.rb
+++ ruby/sudoku_project/tile.rb
@@ -1,1 +1,18 @@
 require "colorize"
+
+class Tile
+	attr_reader :value
+
+	def initialize(value, revealed = false)
+		@value = value
+		@revealed = revealed
+	end
+
+	def to_s
+		revealed? ? value.to_s.colorize(:red) : "-"
+	end
+
+	def revealed?
+		@revealed
+	end
+end
