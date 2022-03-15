ddf9d9dc22a1760cc4692d44f95cb083d0faf813
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-15 13:25:34 -0700

    Add class methods to get replies via user_id and question_id

--- sql/aa_questions_project/reply.rb
+++ sql/aa_questions_project/reply.rb
@@ -3,8 +3,8 @@ require_relative 'questions_database'
 class Reply
 	attr_accessor :id, :question_id, :parent_reply_id, :author_id, :body
 
-	def self.find_by_id(id)
-		reply = QuestionsDatabase.instance.get_first_row(<<-SQL, id)
+	def self.find_by_id(reply_id)
+		reply = QuestionsDatabase.instance.get_first_row(<<-SQL, reply_id)
 			SELECT *
 			FROM replies
 			WHERE id = ?
@@ -12,6 +12,28 @@ class Reply
 		return nil if reply.nil?
 
 		Reply.new(reply)
+	end
+
+	def self.find_by_user_id(user_id)
+		replies = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+			SELECT *
+			FROM replies
+			WHERE author_id = ?
+		SQL
+		return nil unless replies.length > 0
+
+		replies.map { |reply| Reply.new(reply) }
+	end
+
+	def self.find_by_question_id(question_id)
+		replies = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+			SELECT *
+			FROM replies
+			WHERE question_id = ?
+		SQL
+		return nil unless replies.length > 0
+
+		replies.map { |reply| Reply.new(reply) }
 	end
 
 	def initialize(options)
