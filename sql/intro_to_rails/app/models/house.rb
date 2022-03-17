f2c3617d7cf12bc4325b845cfd48926db5234e78
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-16 23:53:21 -0700

    Add rails validation for house and person model

--- sql/intro_to_rails/app/models/house.rb
+++ sql/intro_to_rails/app/models/house.rb
@@ -1,3 +1,13 @@
 class House < ApplicationRecord
-	
+	validates :address, presence: true
 end
+# == Schema Information
+#
+# Table name: houses
+#
+#  id         :integer(8)      not null, primary key
+#  address    :string          not null
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
--- sql/intro_to_rails/app/models/person.rb
+++ sql/intro_to_rails/app/models/person.rb
@@ -1,3 +1,14 @@
 class Person < ApplicationRecord
-
+	validates :name, presence: true
 end
+# == Schema Information
+#
+# Table name: people
+#
+#  id         :integer(8)      not null, primary key
+#  name       :string          not null
+#  house_id   :integer(4)      not null
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
