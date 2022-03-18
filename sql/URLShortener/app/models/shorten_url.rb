1215bf8d5f2ed6948c07ca9483c298f8516047a8
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-17 19:51:33 -0700

    Create associations to get submitter and submitted_urls

--- sql/URLShortener/app/models/shorten_url.rb
+++ sql/URLShortener/app/models/shorten_url.rb
@@ -4,16 +4,21 @@ class ShortenUrl < ApplicationRecord
 	validates :short_url, :long_url, :submitter_id, presence: true
 	validates :short_url, uniqueness: true
 
+	belongs_to :submitter, 
+		class_name: :User,
+		foreign_key: :submitter_id,
+		primary_key: :id
+
 	def self.random_code 	
 		loop do 
 			random_code = SecureRandom.urlsafe_base64
-			return random_code unless ShortenedUrl.exists?(short_url: random_code)
+			return random_code unless ShortenUrl.exists?(short_url: random_code)
 		end
 	end
 
 	def self.generate_shorten_url(user, long_url)
 		ShortenUrl.create!(
-			short_url: ShortenUrl.random_code
+			short_url: ShortenUrl.random_code,
 			long_url: long_url,
 			submitter_id: user.id
 		)
--- sql/URLShortener/app/models/user.rb
+++ sql/URLShortener/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
 	validates :email, presence: true, uniqueness: true
+
+	has_many :submitted_urls,
+		class_name: :ShortenUrl,
+		foreign_key: :submitter_id,
+		primary_key: :id
 end
