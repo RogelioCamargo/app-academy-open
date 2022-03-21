4cceed3e5ac6a52f1987872536c359e412edffa5
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-20 20:25:55 -0700

    Change users table and model so that it has only one unqiue username column

---
+++ rails/ArtShare/db/migrate/20220321031205_change_username_from_users.rb
@@ -0,0 +1,6 @@
+class ChangeUsernameFromUsers < ActiveRecord::Migration[7.0]
+  def change
+		change_column :users, :username, :string, null: false
+		add_index :users, :username, unique: true
+  end
+end
