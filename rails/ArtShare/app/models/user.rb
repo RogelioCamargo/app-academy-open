ef632fd048123f09e6133b5e3ee9ce1f7620cda7
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-21 09:04:04 -0700

    Create artwork_shares table and model

--- rails/ArtShare/app/models/user.rb
+++ rails/ArtShare/app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
 	has_many :artworks,
 		class_name: :Artwork,
 		foreign_key: :artist_id,
-		primary_key: id,
-		dependent: :destory
+		primary_key: :id,
+		dependent: :destroy
+
+	has_many :artwork_shares,
+		class_name: :ArtworkShare,
+		foreign_key: :viewer_id,
+		primary_key: :id,
+		dependent: :destroy
+
+	has_many :shared_artworks,
+		through: :artwork_shares,
+		source: :artwork
 end
