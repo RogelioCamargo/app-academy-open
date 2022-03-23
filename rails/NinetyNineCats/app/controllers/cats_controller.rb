4eaf2ff03f25cb8b3fa988825309a4f6557ed45d
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-23 12:37:31 -0700

    Create form to create and edit cat instance

--- rails/NinetyNineCats/app/controllers/cats_controller.rb
+++ rails/NinetyNineCats/app/controllers/cats_controller.rb
@@ -12,4 +12,42 @@ class CatsController < ApplicationController
 			render json: @cat.errors.full_messages, status: :unprocessable_entity
 		end
 	end
+
+	def new
+		@cat = Cat.new
+		render :new
+	end
+
+	def create 
+		@cat = Cat.new(cat_params)
+		if @cat.save
+			redirect_to cat_url(@cat)
+			# render json: @cat
+		else
+			flash.now[:errors] = @cat.errors.full_messages
+			render :new
+			# render json: @cat.errors.full_messages
+		end
+	end
+
+	def edit
+		@cat = Cat.find(params[:id])
+		render :edit
+	end
+
+	def update
+		@cat = Cat.find(params[:id])
+		if @cat.update(cat_params)
+			redirect_to cat_url(@cat)
+		else 
+			flash.now[:errors] = @cat.errors.full_messages
+			render :edit
+		end
+	end
+
+	private 
+
+	def cat_params
+		params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+	end
 end
