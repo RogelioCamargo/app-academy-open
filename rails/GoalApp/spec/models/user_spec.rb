442f97928cf8f37313f3c276c514bb41e37c3a17
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-30 16:02:36 -0700

    Write User instance method tests

--- rails/GoalApp/spec/models/user_spec.rb
+++ rails/GoalApp/spec/models/user_spec.rb
@@ -18,5 +18,35 @@ RSpec.describe User, type: :model do
 		expect(user.session_token).to_not be_nil 
 	end 
 
-	
+	describe "instance methods" do 
+		describe "#is_password?" do 
+			it "verifies a password is correct" do 
+				expect(user.is_password?("sakura")).to be(true)
+			end 
+
+			it "verifies a password is incorrect" do 
+				expect(user.is_password?("rock_lee")).to be(false)
+			end 
+		end 
+
+		describe "#reset_session_token" do 
+			it "sets a new session token on the user" do 
+				user.valid? 
+				old_session_token = user.session_token
+				user.reset_session_token! 
+
+				expect(user.session_token).to_not eq(old_session_token)
+			end 
+
+			it "returns a new session token" do 
+				expect(user.reset_session_token!).to eq(user.session_token)
+			end 
+		end 
+	end
+
+	# describe "class methods" do 
+	# 	describe "::find_by_credentials" do 
+
+	# 	end 
+	# end 
 end
