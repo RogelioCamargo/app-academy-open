e636df67a099de008729dfff4c61b6d2383260f0
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-31 07:41:07 -0700

    Update routes

--- rails/GoalApp/config/routes.rb
+++ rails/GoalApp/config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 	resources :users, only: %i(create new show)
+	resource :session, only: %i(create destroy new)
 end
