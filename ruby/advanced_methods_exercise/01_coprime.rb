bef74ec1c194aab6a2614a781d3e3736fede709f
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-13 17:40:27 -0700

    Complete exercise 1

--- ruby/advanced_methods_exercise/01_coprime.rb
+++ ruby/advanced_methods_exercise/01_coprime.rb
@@ -3,6 +3,20 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+def coprime?(a, b) 
+	# (2..a).each do |value|
+	# 	if (a % value == 0 && b % value == 0) 
+	# 		return false
+	# 	end
+	# end
+
+	# true
+
+	(2..a).none? do |divisor|
+		a % divisor == 0 && b % divisor == 0
+	end
+end
+
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
