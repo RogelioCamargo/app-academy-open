faf9b64b803c67dcb803a7206e5258b3621776a3
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-14 16:04:07 -0700

    Complete largest_prime_factor exercise

--- software-engineering-foundations/debugging_exercise_2/lib/problems.rb
+++ software-engineering-foundations/debugging_exercise_2/lib/problems.rb
@@ -1,3 +1,19 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+
+def largest_prime_factor(number)
+	div = number
+	while div >= 2 do
+		return div if number % div == 0 && is_prime?(div)
+		div -= 1
+	end
+end
+
+def is_prime?(number) 
+	(2...number).each do |value|
+		return false if number % value == 0
+	end
+
+	true
+end
