552196a55cf10688312e8b21d5ed1c240ef13399
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-14 18:51:56 -0700

    Complete dog project

--- software-engineering-foundations/dog_project/lib/dog.rb
+++ software-engineering-foundations/dog_project/lib/dog.rb
@@ -1,3 +1,46 @@
 class Dog
+	def initialize(name, breed, age, bark, favorite_foods)
+		@name = name
+		@breed = breed 
+		@age = age 
+		@bark = bark 
+		@favorite_foods = favorite_foods
+	end
 
+	def name 
+		@name
+	end
+
+	def breed 
+		@breed 
+	end
+
+	def age 
+		@age 
+	end
+
+	def age=(number)
+		@age = number 
+	end
+
+	def bark
+		if @age <= 3 
+			@bark.downcase
+		else
+			@bark.upcase
+		end
+	end
+
+	def favorite_foods
+		@favorite_foods
+	end
+
+	def favorite_food?(food)
+		@favorite_foods.each do |favorite_food|
+			if favorite_food.downcase == food.downcase
+				return true
+			end
+		end
+		false
+	end
 end
