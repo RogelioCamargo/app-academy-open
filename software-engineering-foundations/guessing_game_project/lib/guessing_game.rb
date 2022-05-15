e1984c9ebfccc8251eeb70ca6fcea9b736efcfd8
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-15 09:47:02 -0700

    Complete guessing game project

--- software-engineering-foundations/guessing_game_project/lib/guessing_game.rb
+++ software-engineering-foundations/guessing_game_project/lib/guessing_game.rb
@@ -1,37 +1,34 @@
 class GuessingGame
 	def initialize(min, max)
-		@min = min
-		@max = max
 		@secret_num = rand(min..max)
 		@num_attempts = 0
-		@game_over = false
+		@game_over = false 
 	end
 
 	def num_attempts
 		@num_attempts
 	end
 
-	def game_over?
+	def game_over? 
 		@game_over
 	end
 
-	def check_num(guess)
+	def check_num(num)
 		@num_attempts += 1
-
-		if guess > @secret_num
-			puts "too big"
-		elsif guess < @secret_num
-			puts "too small"
-		else
-			@game_over = true
-			puts "you win"
+		if @secret_num > num 
+			puts 'too small'
+		elsif @secret_num < num 
+			puts 'too big'
+		else  
+			puts 'you win'
+			@game_over = true 
 		end
 	end
 
-	def ask_user
-		puts "enter a number"
-
+	def ask_user 
+		puts 'enter a number'
 		guess = gets.chomp.to_i
-		self.check_num(guess)
+		check_num(guess)
 	end
+
 end
