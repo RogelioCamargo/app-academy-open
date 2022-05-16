690f5ac5897f91e31ac078bb8f16b4b3c0ff63dd
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-15 23:06:47 -0700

    Write Board class

--- software-engineering-foundations/battleship_project/lib/board.rb
+++ software-engineering-foundations/battleship_project/lib/board.rb
@@ -1,6 +1,12 @@
 class Board
 	attr_reader :size
 
+	def self.print_grid(grid)
+		grid.each do |row|
+			puts row.join(" ")
+		end
+	end
+
   def initialize(size)
 		@grid = Array.new(size) { Array.new(size).fill(:N) }
 		@size = size * size
@@ -24,5 +30,52 @@ class Board
 			end
 		end
 		count
+	end
+
+	def attack(position) 
+		if self[position] == :S 
+			self[position] = :H
+			puts 'you sunk my battleship!'
+			return true
+		else 
+			self[position] = :X
+			return false 
+		end
+	end
+
+	def place_random_ships 
+		ship_count = self.size / 4
+		n = @grid.length - 1
+		while (ship_count != 0)
+			position = [rand(0..n), rand(0..n)]
+			if self[position] == :N 
+				self[position] = :S
+				ship_count -= 1
+			end
+		end
+	end
+
+	def hidden_ships_grid
+		n = @grid.length
+		new_grid = Array.new(n) { Array.new(n) }
+		(0...n).each do |r|
+			(0...n).each do |c|
+				position = [r, c]
+				if self[position] == :S
+					new_grid[r][c] = :N 
+				else 
+					new_grid[r][c] = self[position]
+				end
+			end
+		end
+		new_grid
+	end
+
+	def cheat 
+		Board.print_grid(@grid)
+	end
+
+	def print
+		Board.print_grid(self.hidden_ships_grid)
 	end
 end
