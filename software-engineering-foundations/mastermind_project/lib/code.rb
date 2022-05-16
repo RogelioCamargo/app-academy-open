41f2978afffe49fd32636989d4dd8877e9e9f192
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-15 17:46:32 -0700

    Complete part 1

--- software-engineering-foundations/mastermind_project/lib/code.rb
+++ software-engineering-foundations/mastermind_project/lib/code.rb
@@ -6,4 +6,39 @@ class Code
     "Y" => :yellow
   }
 
+	attr_reader :pegs 
+
+	def self.random(length)
+		pegs = []
+		length.times { pegs << POSSIBLE_PEGS.keys.sample }
+		Code.new(pegs)
+	end
+
+	def self.from_string(string_pegs)
+		pegs = string_pegs.split("")
+		Code.new(pegs)
+	end
+
+	def self.valid_pegs?(pegs)
+		pegs.each do |peg|
+			return false if !POSSIBLE_PEGS.has_key?(peg.upcase)
+		end
+		true
+	end
+
+	def initialize(pegs)
+		if !Code.valid_pegs?(pegs)
+			raise 'not valid pegs'
+		end 
+
+		@pegs = pegs.map { |peg| peg.upcase }
+	end
+
+	def [](index)
+		@pegs[index]
+	end
+
+	def length
+		@pegs.length
+	end
 end
