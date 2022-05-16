ec6ee330dcd9da14cae0a57c2fd297fde8ead474
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-15 22:09:20 -0700

    Complete board part 1

--- software-engineering-foundations/battleship_project/lib/board.rb
+++ software-engineering-foundations/battleship_project/lib/board.rb
@@ -1,3 +1,28 @@
 class Board
-  
+	attr_reader :size
+
+  def initialize(size)
+		@grid = Array.new(size) { Array.new(size).fill(:N) }
+		@size = size * size
+	end
+
+	def [](position)
+		r, c = position
+		@grid[r][c]
+	end
+
+	def []=(position, value)
+		r, c = position
+		@grid[r][c] = value
+	end
+
+	def num_ships 
+		count = 0
+		(0...@grid.length).each do |r|
+			(0...@grid.length).each do |c|
+				count += 1 if @grid[r][c] == :S
+			end
+		end
+		count
+	end
 end
