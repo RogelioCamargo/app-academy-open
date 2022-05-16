47eb29fdaca6970f6115b9e7156f7deb834e9793
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-15 23:10:57 -0700

    Write Player class

--- software-engineering-foundations/battleship_project/lib/player.rb
+++ software-engineering-foundations/battleship_project/lib/player.rb
@@ -1,3 +1,8 @@
 class Player
-
+	def get_move 
+		puts "enter a position with coordinates separated with a space like `4 7`"
+		guess_position = gets.chomp 
+		row, col = guess_position.split(" ")
+		[row.to_i, col.to_i]
+	end
 end
