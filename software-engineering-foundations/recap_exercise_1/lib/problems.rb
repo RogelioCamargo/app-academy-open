c29b760b8b15330050be1ac542857d61fc62c6f2
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-16 20:10:52 -0700

    Complete recap exercise 1

--- software-engineering-foundations/recap_exercise_1/lib/problems.rb
+++ software-engineering-foundations/recap_exercise_1/lib/problems.rb
@@ -5,8 +5,18 @@
 # Example:
 #
 # all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
-def all_vowel_pairs(words)
+require "byebug"
 
+def all_vowel_pairs(words)
+	matches = []
+	all_vowels = ['a', 'e', 'i', 'o', 'u']
+	(0...words.length).each do |i|
+		(i + 1...words.length).each do |j|
+			pair = words[i] + ' ' + words[j]
+			matches << pair if all_vowels.all? { |vowel| pair.include?(vowel) }
+		end
+	end
+	matches 
 end
 
 
@@ -18,7 +28,11 @@ end
 # composite?(9)     # => true
 # composite?(13)    # => false
 def composite?(num)
-
+	(2...num).each do |i|
+		return true if num % i == 0
+	end
+	
+	false 
 end
 
 
@@ -32,7 +46,9 @@ end
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
-
+	bigrams.filter do |bigram|
+		str.include?(bigram)
+	end
 end
 
 class Hash
@@ -50,7 +66,12 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
-
+			prc ||= Proc.new { |k, v| k == v }
+			matches = Hash.new 
+			self.each do |k, v|
+				matches[k] = v if prc.call(k, v)
+			end
+			matches
     end
 end
 
@@ -64,7 +85,14 @@ class String
     # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
     # "cats".substrings(2)  # => ["ca", "at", "ts"]
     def substrings(length = nil)
-
+			substrings = []
+			(0...self.length).each do |i|
+				(i...self.length).each do |j|
+					substring = self[i..j]
+					substrings << substring if length == nil || substring.length == length
+				end
+			end
+			substrings
     end
 
 
@@ -78,6 +106,12 @@ class String
     # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
     # "zebra".caesar_cipher(4)    #=> "difve"
     def caesar_cipher(num)
-
+			alphabet = ("a".."z").to_a
+			encoded = Array.new(self.length)
+			self.each_char.with_index do |char, index|
+				og_index = alphabet.index(char)
+				encoded[index] = alphabet[(og_index + num) % 26]
+			end
+			encoded.join("")
     end
 end
