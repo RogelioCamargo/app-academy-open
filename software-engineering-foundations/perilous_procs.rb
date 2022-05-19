2b991abb05ffa78f0d6627399675213cec1b2a9f
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-19 08:53:50 -0700

    Complete phase 2

--- software-engineering-foundations/perilous_procs.rb
+++ software-engineering-foundations/perilous_procs.rb
@@ -121,9 +121,97 @@ def first_index(array, &prc)
 	nil
 end
 
-p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
-p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
-p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
-p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
-p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
-p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }    				# nil
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.length > 3 }           # 2
+# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 3 } # 0
+# p first_index(['bitten', 'bit', 'cat', 'byte', 'below']) { |el| el.length > 6 } # nil
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 'b' }            # 0
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el.include?('a') }        # 1
+# p first_index(['bit', 'cat', 'byte', 'below']) { |el| el[0] == 't' }    				# nil
+
+def xnor_select(array, prc_1, prc_2) 
+	selected = []
+	array.each do |el|
+		result_1 = prc_1.call(el)
+		result_2 = prc_2.call(el)
+		if result_1 && result_2 || !result_1 && !result_2
+			selected << el  
+		end
+	end
+
+	selected
+end
+
+# is_even = Proc.new { |n| n % 2 == 0 }
+# is_odd = Proc.new { |n| n % 2 != 0 }
+# is_positive = Proc.new { |n| n > 0 }
+# p xnor_select([8, 3, -4, -5], is_even, is_positive)         # [8, -5]
+# p xnor_select([-7, -13, 12, 5, -10], is_even, is_positive)  # [-7, -13, 12]
+# p xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)   # [5, -10]
+
+def filter_out!(array, &prc)
+	i = array.length - 1
+	while i >= 0 
+		array.delete_at(i) if prc.call(array[i])
+		i -= 1
+	end
+
+	array
+end
+
+# arr_1 = [10, 6, 3, 2, 5 ]
+# filter_out!(arr_1) { |x| x.odd? }
+# p arr_1     # [10, 6, 2]
+
+# arr_2 = [1, 7, 3, 5 ]
+# filter_out!(arr_2) { |x| x.odd? }
+# p arr_2     # []
+
+# arr_3 = [10, 6, 3, 2, 5 ]
+# filter_out!(arr_3) { |x| x.even? }
+# p arr_3     # [3, 5]
+
+# arr_4 = [1, 7, 3, 5 ]
+# filter_out!([1, 7, 3, 5 ]) { |x| x.even? }
+# p arr_4 # [1, 7, 3, 5]
+
+def multi_map(array, num = 1, &prc)
+	new_array = array[0..-1]
+	num.times do
+		(0...array.length).each do |i|
+			new_array[i] = prc.call(new_array[i])
+		end
+	end
+
+	new_array
+end
+
+# p multi_map(['pretty', 'cool', 'huh?']) { |s| s + '!'}      # ["pretty!", "cool!", "huh?!"]
+# p multi_map(['pretty', 'cool', 'huh?'], 1) { |s| s + '!'}   # ["pretty!", "cool!", "huh?!"]
+# p multi_map(['pretty', 'cool', 'huh?'], 3) { |s| s + '!'}   # ["pretty!!!", "cool!!!", "huh?!!!"]
+# p multi_map([4, 3, 2, 7], 1) { |num| num * 10 }             # [40, 30, 20, 70]
+# p multi_map([4, 3, 2, 7], 2) { |num| num * 10 }             # [400, 300, 200, 700]
+# p multi_map([4, 3, 2, 7], 4) { |num| num * 10 }             # [40000, 30000, 20000, 70000]
+
+def proctition(array, &prc)
+	passed = []
+	failed = []
+
+	array.each do |el|
+		if prc.call(el)
+			passed << el  
+		else  
+			failed << el 
+		end
+	end
+
+	passed + failed
+end
+
+p proctition([4, -5, 7, -10, -2, 1, 3]) { |el| el > 0 }
+# [4, 7, 1, 3, -5, -10, -2]
+
+p proctition([7, 8, 3, 6, 10]) { |el| el.even? }
+# [8, 6, 10, 7, 3]
+
+p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
+# ["boot", "bug", "boat", "cat", "dog"]
