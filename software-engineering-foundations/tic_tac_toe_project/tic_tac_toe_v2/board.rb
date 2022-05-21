b2128f18e0c77a8cee8d9c2af7f1a5d44be38dad
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-20 18:40:12 -0700

    Make board dynamic

---
+++ software-engineering-foundations/tic_tac_toe_project/tic_tac_toe_v2/board.rb
@@ -0,0 +1,69 @@
+class Board 
+	attr_reader :size
+
+	def initialize(size)
+		@size = size
+		@grid = Array.new(size) { Array.new(size).fill('_') }
+	end
+
+	def reset
+		@grid = Array.new(@size) { Array.new(@size).fill('_') }
+	end
+
+	def valid?(position)
+		x, y = position
+		x < size && x >= 0 && y < size && y >= 0
+	end
+
+	def empty?(position)
+		x, y = position
+		@grid[x][y] == '_'
+	end	
+
+	def place_mark(position, mark)
+		raise "Invalid Position" if !valid?(position)
+		raise "Not Empty" if !empty?(position)
+
+		x, y = position
+		@grid[x][y] = mark
+	end
+
+	def print 
+		@grid.each do |row|
+			p row.join(" ")
+		end
+	end
+
+	def win_row?(mark)
+		(0...size).each do |row|
+			return true if (0...size).all? { |col| @grid[row][col] == mark }
+		end
+		false
+	end
+
+	def win_col?(mark)
+		(0...size).each do |col|
+			return true if (0...size).all? { |row| @grid[row][col] == mark }
+		end
+		false 
+	end
+
+	def win_diagonal?(mark)
+		return true if (0...size).all? { |i| @grid[i][i] == mark }
+		reversed_grid = @grid.reverse
+		p reversed_grid
+		return true if (0...size).all? { |i| reversed_grid[i][i] == mark }
+		false 
+	end
+
+	def win?(mark)
+		win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+	end
+
+	def empty_positions?
+		(0...size).each do |row|
+			return true if (0...size).any? { |col| @grid[row][col] == '_' }
+		end
+		false
+	end
+end
