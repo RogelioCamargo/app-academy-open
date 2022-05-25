eec780e6d96320b703676f1aa85e518a95a7e71e
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-24 21:56:00 -0700

    Complete phase 3 and 4

--- ruby/word_chain_project/word_chainer.rb
+++ ruby/word_chain_project/word_chainer.rb
@@ -20,13 +20,14 @@ class WordChainer
 
 	def run(source, target)
 		@current_words = [source]
-		@all_seen_words = Set.new([source])
+		@all_seen_words = { source => nil }
 
 		until @current_words.empty? 
 			new_current_words = explore_current_words(@current_words, @all_seen_words)
-			p new_current_words
 			@current_words = new_current_words
 		end
+
+		build_path(target, @all_seen_words)
 	end 
 
 	def explore_current_words(current_words, all_seen_words)
@@ -34,15 +35,28 @@ class WordChainer
 			current_words.each do |current_word|
 				adjacent_words = adjacent_words(current_word)
 				adjacent_words.each do |adjacent_word|
-					next if all_seen_words.include?(adjacent_word)
+					next if all_seen_words.has_key?(adjacent_word)
 
 					new_current_words << adjacent_word
-					all_seen_words.add(adjacent_word)
+					all_seen_words[adjacent_word] = current_word
 				end
 			end
 		new_current_words
 	end
 
+	def build_path(target, all_seen_words)
+		path = [target]
+		previous_word = all_seen_words[target] 
+		until previous_word.nil? 
+			path << previous_word
+			previous_word = all_seen_words[previous_word]
+		end
+		path.reverse
+	end
+
 	private 
 	attr_reader :dictionary 
 end
+
+wc = WordChainer.new("./dictionary.txt")
+p wc.run("cat", "run")
