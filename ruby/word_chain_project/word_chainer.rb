dab8aeda0b0fd47f5583fd1c17717e932a6910f8
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-24 20:49:53 -0700

    Complete phase 1 and 2

--- ruby/word_chain_project/word_chainer.rb
+++ ruby/word_chain_project/word_chainer.rb
@@ -1,74 +1,48 @@
 require "set"
-
-class WordChainer
-	ALPHABET = ("a".."z").to_a
-
+class WordChainer 
 	def initialize(filename)
-		words = File.readlines(filename).map(&:chomp)	
+		words = File.readlines(filename).map(&:chomp)
 		@dictionary = Set.new(words)
 	end
 
-	def run(source, target)
-		raise "source or target word not found in dictory" if !dictionary.include?(source) || !dictionary.include?(target)
-		
-		@current_words, @all_seen_words = [source], { source => nil }
-		
-		until @current_words.empty? || @all_seen_words.include?(target)
-      explore_current_words
-    end
-
-    build_path(target)
-	end
-
-	private
-
-	attr_reader :dictionary
-
 	def adjacent_words(word)
-		words = []
-		word.each_char.with_index do |old_char, i|
-			ALPHABET.each do |new_char|
-				next if old_char == new_char
-
+		adjacent_words = []
+		word.each_char.with_index do |char, index|
+			("a"..."z").each do |new_char|
+				next if char.downcase == new_char
 				new_word = word[0..-1]
-				new_word[i] = new_char
-				words << new_word if dictionary.include?(new_word) 
+				new_word[index] = new_char
+				adjacent_words << new_word if dictionary.include?(new_word)
 			end
 		end
-
-		words
+		adjacent_words
 	end
 
-	def explore_current_words
-    new_current_words = []
-    @current_words.each do |current_word|
-			# p current_word + " "
-      adjacent_words(current_word).each do |adjacent_word|
-        next if @all_seen_words.key?(adjacent_word)
+	def run(source, target)
+		@current_words = [source]
+		@all_seen_words = Set.new([source])
 
-        new_current_words << adjacent_word
-        @all_seen_words[adjacent_word] = current_word
-      end
-    end
+		until @current_words.empty? 
+			new_current_words = explore_current_words(@current_words, @all_seen_words)
+			p new_current_words
+			@current_words = new_current_words
+		end
+	end 
 
-    @current_words = new_current_words
-  end
+	def explore_current_words(current_words, all_seen_words)
+		new_current_words = []
+			current_words.each do |current_word|
+				adjacent_words = adjacent_words(current_word)
+				adjacent_words.each do |adjacent_word|
+					next if all_seen_words.include?(adjacent_word)
 
-  def build_path(target)
-    path = []
-    current_word = target
-    until current_word.nil?
-      path << current_word
-      current_word = @all_seen_words[current_word]
-    end
+					new_current_words << adjacent_word
+					all_seen_words.add(adjacent_word)
+				end
+			end
+		new_current_words
+	end
 
-    path.reverse
-  end
-
-end
-
-if $PROGRAM_NAME == __FILE__
-  # provide file name on command line
-	filename, word_one, word_two = ARGV
-  p WordChainer.new(filename).run(word_one, word_two)
+	private 
+	attr_reader :dictionary 
 end
