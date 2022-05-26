5a915cb76ed95013ba4cbfa0fbd18b8cdc557efa
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-26 06:09:55 -0700

    Write tile class

---
+++ ruby/minesweeper_project/tile.rb
@@ -0,0 +1,88 @@
+class Tile 
+	DELTAS = [
+		[0, 1], [1, 0], [0, -1], [-1, 0],
+		[-1, -1], [-1, 1], [1, -1], [1, 1]
+	].freeze
+	
+	attr_reader :position, :board
+
+	def initialize(board, position)
+		@board = board 
+		@position = position
+		@bombed, @flagged, @explored = false, false, false 
+	end 
+
+	def bombed? 
+		@bombed
+	end
+
+	def flagged? 
+		@flagged
+	end
+
+	def explored? 
+		@explored
+	end
+
+	def plant_bomb
+		@bombed = true
+	end
+
+	def toggle_flag 
+		@flagged = !flagged? unless explored?
+	end
+
+	def explore 
+		return if flagged?
+		return if explored?
+
+		@explored = true 
+		if neighbor_bomb_count == 0 && !bombed?
+			neighbors.each { |neighbor| neighbor.explore }
+		end
+	end
+
+	def neighbors 
+		neighbor_coordinates = DELTAS.map do |(dx, dy)|
+			x, y = position
+			[x + dx, y + dy] 
+		end.select do |position|
+			position.each { |coordinate| coordinate.between?(0, board.size) }
+		end
+
+		neighbor_coordinates.map { |position| board[position] }
+	end
+
+	def neighbor_bomb_count 
+		neighbors.select(&:bombed?).count
+	end
+
+	def inspect 
+		{
+			position: position,
+			bombed: bombed?,
+			flagged: flagged?,
+			explored: explored?
+		}.inspect
+	end
+
+	def render 
+		if flagged? 
+			"F"
+		elsif explored?
+			neighbor_bomb_count == 0 ? "_" : neighbor_bomb_count.to_s
+		else 
+			"*"
+		end
+	end
+
+	def reveal 
+		if flagged?
+			bombed? ? "F" : "N"
+		elsif bombed? 
+			explored? ? "X" : "B"
+		else 
+			neighbor_bomb_count == 0 ? "_" : neighbor_bomb_count.to_s
+		end
+	end
+end
