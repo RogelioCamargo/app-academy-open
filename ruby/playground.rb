1c32abec0db4625ba65054eb8b431cd83000ea56
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-28 07:44:03 -0700

    Rename project

--- ruby/playground.rb
+++ ruby/playground.rb
@@ -1,8 +1,37 @@
-# some_array = [1]
-# some_hash = { some_array => 'secrets' }
-# some_array << 2
-# p some_hash
-# p some_hash[some_array] #=> ???
+class Person 
+	def initialize(name)
+		@name = name 
+	end
 
-puts $PROGRAM_NAME
-puts ARGV
+	# def call_protected_method 
+	# 	self.protected_method_here
+	# end
+
+	def call_private_method
+		puts self.private_method_here(1, 1)
+	end
+
+	private 
+	def private_method_here(a, b) 
+		a + b
+	end
+
+	# protected 
+	# def protected_method_here 
+	# 	puts "this is a protected method"
+	# end
+end
+
+class AnotherPerson < Person 
+	def initialize(name)
+		super
+	end
+end
+
+p = Person.new("Roger")
+# p.call_protected_method
+p.call_private_method
+
+ap = AnotherPerson.new("Emma")
+# ap.call_protected_method
+ap.call_private_method
