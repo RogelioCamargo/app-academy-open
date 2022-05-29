fbf33a5ac7df5191f40e9b95479f4b17e975904e
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-29 10:37:40 -0700

    Write stepable module

---
+++ ruby/chess_project/pieces/stepable.rb
@@ -0,0 +1,23 @@
+module Stepable 
+	def moves 
+		moves = []
+		x, y = position
+		move_directions.each do |dx, dy|
+			new_position = [x + dx, y + dy]
+			next unless board.valid_position?(new_position)
+
+			if board.empty?(new_position)
+				moves << new_position
+			elsif board[new_position].color != color 
+				moves << new_position 
+			end
+		end
+		moves
+	end 
+
+	private 
+
+	def all_possible_moves  
+		raise NotImplementedError
+	end
+end
