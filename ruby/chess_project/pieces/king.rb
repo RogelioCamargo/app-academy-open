78bce56e53caf5b41c14918d0d75fa951921c9c3
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-29 11:05:04 -0700

    Write king class

---
+++ ruby/chess_project/pieces/king.rb
@@ -0,0 +1,19 @@
+require_relative "piece"
+require_relative "stepable"
+
+class King < Piece
+	include Stepable
+
+	def symbol 
+		'♚'.colorize(color)
+	end
+
+	private 
+
+	def all_possible_moves
+		[
+			[-1,-1], [-1,0], [-1,1], [0,-1],
+			[0,1], [1,-1], [1,0], [1,1]
+		]
+	end
+end
