e5f05e36ea4e46dfcc708ff99a4595c382d98d89
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-05-30 16:36:18 -0700

    Write player parent class and human player child class

---
+++ ruby/chess_project/human_player.rb
@@ -0,0 +1,20 @@
+require_relative "player"
+
+class HumanPlayer < Player 
+	def make_move
+		start_position, end_position = nil, nil 
+		until start_position && end_position
+			display.render 
+
+			if start_position
+				puts "#{color}'s turn. Move to where?"
+				end_position = display.cursor.get_input
+
+			else
+				puts "#{color}'s turn. Move from where?"
+				start_position = display.cursor.get_input
+			end
+		end
+		[start_position, end_position]
+	end
+end
