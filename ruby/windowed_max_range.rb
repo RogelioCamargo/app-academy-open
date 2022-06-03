9a92539c102aec29c673c0a0500af46a722f5bb6
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-03 07:53:25 -0700

    Complete phase 4

--- ruby/windowed_max_range.rb
+++ ruby/windowed_max_range.rb
@@ -13,10 +13,10 @@ def windowed_max_range(array, size)
 	current_max_range
 end
 
-p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
-p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
-p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
-p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
 
 class MyQueue
 	def initialize
@@ -73,3 +73,49 @@ class MyStack
 		@store.length 
 	end
 end
+
+class StackQueue 
+	def initialize
+		@entry = []
+		@depart = [] 
+	end
+
+	def enqueue(item)
+		@entry << item 
+	end
+
+	def dequeue
+		return nil if empty?
+		@entry.length.times { @depart << @entry.pop } if @depart.empty? 
+		@depart.pop 
+	end
+
+	def empty? 
+		@entry.empty? && @depart.empty?
+	end
+
+	def size 
+		@entry.length + @depart.length
+	end
+end
+
+# q = StackQueue.new 
+# q.enqueue(1)
+# q.enqueue(2)
+# q.enqueue(3)
+# q.enqueue(4)
+# q.enqueue(5)
+# p q
+# p q.dequeue
+# p q
+# q.enqueue(6)
+# q.enqueue(7)
+# p q
+# p q.dequeue
+# p q.dequeue
+# p q
+# p q.dequeue
+# p q.dequeue
+# p q
+# p q.dequeue
+# p q
