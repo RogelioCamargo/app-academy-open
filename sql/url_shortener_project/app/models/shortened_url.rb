a6d6fbf4fea507f99a39a20a6930b34b72d1b498
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-11 22:00:10 -0700

    Complete phase 6

--- sql/url_shortener_project/app/models/shortened_url.rb
+++ sql/url_shortener_project/app/models/shortened_url.rb
@@ -1,6 +1,7 @@
 class ShortenedUrl < ApplicationRecord
 	validates :long_url, :short_url, presence: true 
 	validates :short_url, uniqueness: true 
+	validate :no_spamming, :nonpremium_max
 
 	belongs_to :submitter, 
 		primary_key: :id, 
@@ -56,4 +57,28 @@ class ShortenedUrl < ApplicationRecord
 			.where('created_at > ?', 10.minutes.ago)
 			.distinct.count
 	end
+
+	private
+
+	def no_spamming
+    last_minute = ShortenedUrl
+      .where('created_at >= ?', 1.minute.ago)
+      .where(user_id: user_id)
+      .length
+
+    errors[:maximum] << 'of five short urls per minute' if last_minute >= 5
+  end
+
+  def nonpremium_max
+    return if User.find(self.user_id).premium
+
+    number_of_urls =
+      ShortenedUrl
+        .where(user_id: user_id)
+        .length
+
+    if number_of_urls >= 5
+      errors[:Only] << 'premium members can create more than 5 short urls'
+    end
+  end
 end
