a0eb5cc940996918eb54d7ca6caefb79fa736511
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-14 13:57:32 -0700

    Fix bugs in models

--- sql/polls_project/app/models/response.rb
+++ sql/polls_project/app/models/response.rb
@@ -1,9 +1,9 @@
 class Response < ApplicationRecord
-	belongs_to :answer_choice
+	belongs_to :answer_choice,
 		primary_key: :id, 
 		foreign_key: :answer_choice_id, 
 		class_name: :AnswerChoice
-		
+
 	belongs_to :respondent,
 		primary_key: :id, 
 		foreign_key: :respondent_id, 
