c0e18a2804ec2060a1480231ae62fd89570819ab
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-14 12:17:39 -0700

    Add associations to question model

--- sql/polls_project/app/models/question.rb
+++ sql/polls_project/app/models/question.rb
@@ -1,3 +1,13 @@
 class Question < ApplicationRecord
 	validates :text, presence: true
+
+	has_many :answer_choices, 
+		primary_key: :id, 
+		foreign_key: :answer_choice_id, 
+		class_name: :AnswerChoice
+
+	belongs_to :poll, 
+		primary_key: :id, 
+		foreign_key: :poll_id, 
+		class_name: :Poll
 end
