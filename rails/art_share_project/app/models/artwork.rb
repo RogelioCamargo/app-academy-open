aeec6f57f4ae6e8be5489e80010146f805f04898
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-19 17:55:43 -0700

    Create comments table and model

--- rails/art_share_project/app/models/artwork.rb
+++ rails/art_share_project/app/models/artwork.rb
@@ -13,6 +13,9 @@ class Artwork < ApplicationRecord
 	has_many :shared_viewers, 
 		through: :artwork_shares,
 		source: :viewer
+	
+	has_many :comments, 
+		dependent: :destroy
 
 	def self.artworks_for_user_id(user_id)
 		Artwork
---
+++ rails/art_share_project/app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+	validates :body, presence: true 
+	
+	belongs_to :author, 
+		class_name: :User 
+
+	belongs_to :artwork 
+end
