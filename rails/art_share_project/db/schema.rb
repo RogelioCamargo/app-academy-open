8404cd6a91044dee1751966954e7eccd42ce9a03
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-20 08:33:10 -0700

    Write artwork_collections table and model

--- rails/art_share_project/db/schema.rb
+++ rails/art_share_project/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_145007) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_153038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artwork_collections", force: :cascade do |t|
+    t.integer "collection_id", null: false
+    t.integer "artwork_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "artwork_shares", force: :cascade do |t|
     t.integer "artwork_id", null: false
