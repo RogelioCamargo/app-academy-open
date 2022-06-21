ebd88280b5977117d9e6718e84f2e30a3d1c9b9e
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-21 08:31:44 -0700

    Create cat_rental_requests table and model

---
+++ rails/ninetynine_cats_project/app/models/cat_rental_request.rb
@@ -0,0 +1,42 @@
+class CatRentalRequest < ApplicationRecord
+	STATUS_STATES = %w(APPROVED DENIED PENDING).freeze
+	validates :start_date, :end_date, :status, presence: true
+	validates :status, inclusion: STATUS_STATES
+	validate :does_not_overlap_approved_request
+
+	belongs_to :cat
+
+	def approved?
+    self.status == 'APPROVED'
+  end
+
+  def denied?
+    self.status == 'DENIED'
+  end
+
+  def pending?
+    self.status == 'PENDING'
+  end
+
+	private 
+	
+	def overlapping_requests
+		CatRentalRequest
+      .where.not(id: self.id)
+      .where(cat_id: cat_id)
+      .where.not('start_date > :end_date OR end_date < :start_date',
+                 start_date: start_date, end_date: end_date)
+	end
+
+	def overlapping_approved_requests
+		overlapping_requests.where('status = \'APPROVED\'')
+	end
+
+	def does_not_overlap_approved_request
+		return if self.denied?
+
+    unless overlapping_approved_requests.empty?
+      errors.add(:base, 'Request conflicts with existing approved request')
+    end
+	end
+end
--- rails/ninetynine_cats_project/db/schema.rb
+++ rails/ninetynine_cats_project/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_001851) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_134111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cat_rental_requests", force: :cascade do |t|
+    t.integer "cat_id", null: false
+    t.date "start_date", null: false
+    t.date "end_date", null: false
+    t.string "status", default: "PENDING", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cat_id"], name: "index_cat_rental_requests_on_cat_id"
+  end
 
   create_table "cats", force: :cascade do |t|
     t.date "birth_date", null: false
