8b9efccc99427118f396ed51d3bce082b5b1b7a1
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-20 21:11:26 -0700

    Create new and edit forms for creating/updating a cat

--- rails/ninetynine_cats_project/app/controllers/cats_controller.rb
+++ rails/ninetynine_cats_project/app/controllers/cats_controller.rb
@@ -5,11 +5,45 @@ class CatsController < ApplicationController
 	end
 
 	def show 
-		@cat = Cat.find(params[:id])
+		@cat = Cat.find_by(id: params[:id])
 		if @cat.nil? 
 			redirect_to cats_url
 		else 
 			render :show
 		end
+	end 
+
+	def new 
+		@cat = Cat.new 
+		render :new
+	end
+
+	def edit 
+		@cat = Cat.find_by(id: params[:id])
+		render :edit
+	end
+
+	def create 
+		@cat = Cat.new(cat_params)
+		if @cat.save 
+			redirect_to cat_url(@cat)
+		else 
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	def update 
+		@cat = Cat.find(params[:id])
+		if @cat.update(cat_params)
+			redirect_to cat_url(@cat)
+		else
+			render :edit, status: :unprocessable_entity
+		end
+	end
+
+	private 
+
+	def cat_params
+		params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
 	end
 end
