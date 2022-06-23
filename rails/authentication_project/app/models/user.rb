764b830d3754bc59ca4a79e71b8e3620be14a20e
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-23 08:43:45 -0700

    Add helper methods to user class

--- rails/authentication_project/app/models/user.rb
+++ rails/authentication_project/app/models/user.rb
@@ -5,9 +5,32 @@ class User < ApplicationRecord
 
 	before_validation :ensure_session_token
 
+	attr_reader :password
+
+	def self.find_by_credentials(username, password)
+		user = User.find_by(username: username)
+		return user if BCrypt::Password.new(user.password_digest).is_password?(password)
+		nil
+	end
+
+	def self.generate_session_token
+		SecureRandom::urlsafe_base64(16)
+	end
+
+	def reset_session_token!
+		self.session_token = User.generate_session_token
+		self.save!
+		self.session_token
+	end
+
+	def password=(password)
+		@password = password
+		self.password_digest = BCrypt::Password.create(password)
+	end
+
 	private 
 
 	def ensure_session_token
-		
+		self.session_token ||= User.generate_session_token
 	end
 end
