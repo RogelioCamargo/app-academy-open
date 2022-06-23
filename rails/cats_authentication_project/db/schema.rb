07f9c72804543e14bfe9dc594e9c900489c3e085
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-23 13:16:00 -0700

    Create users table

--- rails/cats_authentication_project/db/schema.rb
+++ rails/cats_authentication_project/db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_07_14_042724) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_06_23_155836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +19,8 @@ ActiveRecord::Schema.define(version: 2017_07_14_042724) do
     t.date "end_date", null: false
     t.date "start_date", null: false
     t.string "status", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["cat_id"], name: "index_cat_rental_requests_on_cat_id"
   end
 
@@ -31,8 +30,18 @@ ActiveRecord::Schema.define(version: 2017_07_14_042724) do
     t.string "name", null: false
     t.string "sex", null: false
     t.text "description"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["password_digest"], name: "index_users_on_password_digest", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
