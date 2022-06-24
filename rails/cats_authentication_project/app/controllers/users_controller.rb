dccac03d3a625e3a02b736dae11767f4a14998d8
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-23 20:11:33 -0700

    Update users controller

--- rails/cats_authentication_project/app/controllers/users_controller.rb
+++ rails/cats_authentication_project/app/controllers/users_controller.rb
@@ -7,9 +7,11 @@ class UsersController < ApplicationController
 	def create 
 		@user = User.new(user_params)
 		if @user.save 
+			login_user(@user)
 			redirect_to cats_url
 		else 
-			render :new
+			flash.now[:errors] = @user.errors.full_messages
+			render :new, status: :unprocessable_entity
 		end
 	end
 
