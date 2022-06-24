10a2e56009dc25843942aee4467eeb5a1d77e420
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-23 20:12:13 -0700

    Create sessions controller and log in view html page

---
+++ rails/cats_authentication_project/app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+	def new 
+		render :new 
+	end
+
+	def create 
+		user = User.find_by_credentials(
+			params[:user][:user_name], 
+			params[:user][:password]
+		)
+
+		if user.nil? 
+			flash.now[:errors] = ["Incorrect username and/or password"]
+			render :new, status: :unprocessable_entity
+		else 
+			login_user!(user)
+			redirect_to new_session_url
+	end
+
+	def destroy 
+		logout_user!
+		redirect_to new_session_url
+	end
+end
