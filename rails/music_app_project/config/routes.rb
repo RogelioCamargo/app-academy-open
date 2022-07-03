f4ccac6f583fc66b552a2d91d0ccab949dc82fd7
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-02 20:24:05 -0700

    Create views for user login and sign up, with a navbar for quick access

--- rails/music_app_project/config/routes.rb
+++ rails/music_app_project/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#show"
 
 	resources :users, only: %i(new create show)
 	resource :session, only: %i(new create destroy)
