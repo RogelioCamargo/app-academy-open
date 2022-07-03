ae4d1c0782af5382615f8bef67b5e297264b22e6
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-02 17:12:44 -0700

    Create user routes and controller for creating new users

---
+++ rails/music_app_project/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+	def new 
+		@user = User.new
+		render :new 
+	end
+
+	def create 
+		@user = User.new(user_params)
+		if user.save
+			login_user(@user)
+			redirect_to user_url(@user)
+		else
+			flash.now[:errors] = @user.errors.full_messages 
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	private 
+
+	def user_params 
+		params.requires(:user).permit(:email, :password)
+	end
+end
