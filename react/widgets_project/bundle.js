6c4a09ae3959194627b2d074b04b50400f6fa65e
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-16 16:41:40 -0700

    Refactor clock widget

--- react/widgets_project/bundle.js
+++ react/widgets_project/bundle.js
@@ -45,78 +45,7 @@ function Clock() {
     setDate(new Date());
   };
 
-  var getDay = function getDay(date) {
-    switch (date.getDay()) {
-      case 0:
-        return "Sunday";
-
-      case 1:
-        return "Monday";
-
-      case 2:
-        return "Tuesday";
-
-      case 3:
-        return "Wednesday";
-
-      case 4:
-        return "Thursday";
-
-      case 5:
-        return "Friday";
-
-      case 6:
-        return "Saturday";
-
-      default:
-        return "Not Found";
-    }
-  };
-
-  var getMonth = function getMonth(date) {
-    switch (date.getMonth()) {
-      case 0:
-        return "January";
-
-      case 1:
-        return "February";
-
-      case 2:
-        return "March";
-
-      case 3:
-        return "April";
-
-      case 4:
-        return "May";
-
-      case 5:
-        return "June";
-
-      case 6:
-        return "July";
-
-      case 7:
-        return "August";
-
-      case 8:
-        return "September";
-
-      case 9:
-        return "October";
-
-      case 10:
-        return "November";
-
-      case 11:
-        return "December";
-
-      default:
-        return "Not Found";
-    }
-  };
-
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "".concat(date.getHours() % 12, ":").concat(date.getMinutes(), ":").concat(date.getSeconds())), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "".concat(getDay(date), " ").concat(date.getDate(), " ").concat(getMonth(date), " ").concat(date.getFullYear())));
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "".concat(date.getHours() % 12, ":").concat(date.getMinutes(), ":").concat(date.getSeconds())), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "".concat(date.toDateString())));
 }
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Clock);
