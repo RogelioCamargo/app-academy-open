61c6c758471717bb590ab4ee45aa2944ccf077f6
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-17 15:14:17 -0700

    Complete job listing project

--- react/job_listing_project/frontend/actions.js
+++ react/job_listing_project/frontend/actions.js
@@ -1,9 +1,11 @@
 export const SWITCH_LOCATION = "SWITCH_LOCATION";
 
-export const selectLocation = (city, jobs) => ({
+const selectLocation = (city, jobs) => ({
 	type: SWITCH_LOCATION,
 	city,
 	jobs,
 });
 
 window.selectLocation = selectLocation;
+
+export default selectLocation;
--- react/job_listing_project/frontend/reducer.js
+++ react/job_listing_project/frontend/reducer.js
@@ -1,10 +1,19 @@
+import { SWITCH_LOCATION } from "./actions";
+
 const initialState = {
-  city: "Please Select", 
-  jobs: []
+	city: "Please Select",
+	jobs: [],
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+	switch (action.type) {
+		case SWITCH_LOCATION:
+			return { city: action.city, jobs: action.jobs };
+		default:
+			return state;
+	}
 };
+
+window.reducer - reducer;
 
 export default reducer;
