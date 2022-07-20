80d517f34b9d1fffa601e5e89ae2632b36b9ffd5
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-20 08:23:06 -0700

    Bridge frontend with rails api and create a todo api util file with ajax methods

--- react/todos_project/frontend/components/app.jsx
+++ react/todos_project/frontend/components/app.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import TodoListContainer from "./todo_list/todo_list_container";
+import { fetchTodos } from "../util/todo_api_util";
 
-const App = () => (
-	<div>
-		<TodoListContainer />
-	</div>
-);
+const App = () => {
+	return (
+		<div>
+			<TodoListContainer />
+		</div>
+	);
+};
 
 export default App;
