46d627d3bdf50db9c575f8480943c7d518160c4a
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-19 21:44:30 -0700

    Create static page controller and view

--- react/todos_project/config/routes.rb
+++ react/todos_project/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "static_pages#root"
 	namespace :api, defaults: { format: :json } do 
 		resources :todos, except: %i(new edit)
 	end
