30ff2c5327f9e0bbef195819854e7e1e6e3741c5
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-25 12:36:10 -0700

    Configure store and ensure state is updated from console

--- react/giphy_exercise/giphy_search.jsx
+++ react/giphy_exercise/giphy_search.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore from './store/store';
-import Root from './components/root';
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 import { fetchSearchGiphys } from "./util/api_util";
+import { receiveSearchGiphys } from "./actions/giphy_actions";
 
-fetchSearchGiphys("puppies").then((res) => console.log(res.data));
+window.fetchSearchGiphys = fetchSearchGiphys;
+window.receiveSearchGiphys = receiveSearchGiphys;
+
+document.addEventListener("DOMContentLoaded", () => {
+	const store = configureStore();
+	window.store = store;
+	const root = document.getElementById("root");
+	ReactDOM.render(<Root store={store} />, root);
+});
+
+// fetchSearchGiphys("puppies").then((res) => console.log(res.data));
--- react/giphy_exercise/store/store.js
+++ react/giphy_exercise/store/store.js
@@ -1,3 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from '../reducers/root_reducer';
+import { createStore } from 'redux';
+import RootReducer from '../reducers/root_reducer';
+
+const configureStore = () => {
+  return createStore(RootReducer);
+};
+
+export default configureStore;
