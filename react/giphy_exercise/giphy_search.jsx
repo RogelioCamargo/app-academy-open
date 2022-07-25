32dc7f8915a9639b6a92b1cc1f8dbdf98ae70c1d
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-25 13:23:34 -0700

    Configure thunk middleware and ensure fetchSearchGiphys thunk action creator works in console

--- react/giphy_exercise/giphy_search.jsx
+++ react/giphy_exercise/giphy_search.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
-import { fetchSearchGiphys } from "./util/api_util";
-import { receiveSearchGiphys } from "./actions/giphy_actions";
+import { receiveSearchGiphys, fetchSearchGiphys } from "./actions/giphy_actions";
 
-window.fetchSearchGiphys = fetchSearchGiphys;
 window.receiveSearchGiphys = receiveSearchGiphys;
+window.fetchSearchGiphys = fetchSearchGiphys;
 
 document.addEventListener("DOMContentLoaded", () => {
 	const store = configureStore();
